40dedc24c0097b5da0fee9d0721eb43996ca8191
season <zh_season@163.com>
2021-11-30 08:46:36 +0800

    rename security sample app name.

--- cement-security/cement-security-sample/src/main/java/cn/edana/cement/security/sample/SecurityApplication.java
+++ cement-security/cement-security-sample/src/main/java/cn/edana/cement/security/sample/SecurityApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SampleApp {
+public class SecurityApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SampleApp.class, args);
+        SpringApplication.run(SecurityApplication.class, args);
     }
 }
